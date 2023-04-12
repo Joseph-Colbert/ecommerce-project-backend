057888700679ec64683219aac0fb33aeb4696c75
Joseph-Colbert <joseph97cm@gmail.com>
2023-04-11 22:12:56 -0400

    Funcion busqueda

--- src/main/java/com/josephcolbert/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/josephcolbert/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.josephcolbert.ecommerce.dao;
 
 import com.josephcolbert.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {  //(entity, primary key)
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable); //select * from products where category_id=?..... simula ese query
 }
